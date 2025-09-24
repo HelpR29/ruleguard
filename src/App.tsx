d3c683a02ec9a70b7f4d03f13ca951a6731b1667
HelpR29 <ragmos69420@gmail.com>
2025-09-24 16:40:16 -0500

    Audits

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import Journal from './pages/Journal';
 import Reports from './pages/Reports';
 import Settings from './pages/Settings';
 import Profile from './pages/Profile';
-import Leaderboard from './pages/Leaderboard';
+import Leaderboard from './pages/LeaderboardFixed';
 import Premium from './pages/Premium';
 import Achievements from './pages/Achievements';
 import Onboarding from './pages/Onboarding';
