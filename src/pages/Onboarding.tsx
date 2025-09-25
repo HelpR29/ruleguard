5d69d28370a3202284c7bae418816577e03baed5
HelpR29 <ragmos69420@gmail.com>
2025-09-25 08:11:10 -0500

    hybrid

--- src/pages/Onboarding.tsx
+++ src/pages/Onboarding.tsx
@@ -81,7 +81,7 @@ export default function Onboarding({ onComplete }: OnboardingProps) {
               Starting Portfolio Amount
             </label>
             <div className="relative">
-              <span className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500">$</span>
+              <span className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-700">$</span>
               <input
                 type="number"
                 value={formData.startingPortfolio}
